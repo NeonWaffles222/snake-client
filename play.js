841650abcb6eb589f3bc5d4195a4830bd6865d79
Noah McMillan <noahm27@gmail.com>
2023-08-02 16:30:39 -0700

    Added input and quit command

--- play.js
+++ play.js
@@ -1,6 +1,22 @@
 const net = require("net");
 const { connect } = require('./client');
+const { stdin } = require("process");
 
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = () => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
 
 console.log("Connecting ...");
 connect();
+
+stdin.on("data", handleUserInput);
