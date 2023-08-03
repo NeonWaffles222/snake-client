2d834d4109aba0455311e60abf990b1036e5f0da
Noah McMillan <noahm27@gmail.com>
2023-08-02 17:03:12 -0700

    Moved input into its own file

--- play.js
+++ play.js
@@ -1,22 +1,7 @@
-const net = require("net");
 const { connect } = require('./client');
-const { stdin } = require("process");
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  return stdin;
-};
-
-const handleUserInput = () => {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
+const { setupInput } = require('./input');
 
 console.log("Connecting ...");
 connect();
 
-stdin.on("data", handleUserInput);
+setupInput();
