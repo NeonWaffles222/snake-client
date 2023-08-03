dfae94cbfd05dc1bffc601fc10531dbe8f0d75e4
Noah McMillan <noahm27@gmail.com>
2023-08-02 17:34:22 -0700

    Added input to move the snake

--- input.js
+++ input.js
@@ -1,6 +1,9 @@
 const { stdin } = require('process');
 
-const setupInput = function() {
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -11,6 +14,20 @@ const setupInput = function() {
 const handleUserInput = (key) => {
   if (key === '\u0003') {
     process.exit();
+  }
+  switch (key) {
+    case 'w':
+      connection.write("Move: up");
+      break;
+    case 'a':
+      connection.write("Move: left");
+      break;
+    case 's':
+      connection.write("Move: down");
+      break;
+    case 'd':
+      connection.write("Move: right");
+      break;
   }
 };
 
